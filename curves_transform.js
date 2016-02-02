3c69b55c742395a44f9a5ad426f3ad226a14bc2c
Mikhail Mangushev <dev.mihnayan@gmail.com>
2016-02-03 00:08:10 +0300

    Deleted unnecessary code from curves_transform.js. The line color of the figure is now specified as an object

--- curves_transform.js
+++ curves_transform.js
@@ -1,34 +1,10 @@
-var figure1 = [
-    [
-        111, 115,
-        141, 103,
-        175, 99,
-        203, 102,
-        223, 108,
-        248, 120,
-        269, 133,
-        289, 148,
-        309, 166
-    ],
-    [
-        309, 166,
-        319, 161,
-        331, 158,
-        346, 157,
-        361, 159
-    ],
-    [
-        361, 159,
-        346, 173,
-        339, 187,
-        337, 208,
-        340, 224
-    ]
-];
-
 var figure11 = [
     {
-        "color": "#eae300",
+        "color": {
+            "r": 0xEA,
+            "g": 0xE3,
+            "b": 0
+        },
         "elements": [
             {
                 "elementType": "curveByPoints",
@@ -51,7 +27,11 @@ var figure11 = [
         ]
     },
     {
-        "color": "#e60000",
+        "color": {
+            "r": 0xE6,
+            "g": 0,
+            "b": 0
+        },
         "elements": [
             {
                 "elementType": "arc",
@@ -60,7 +40,11 @@ var figure11 = [
         ]
     },
     {
-        "color": "#964b95",
+        "color": {
+            "r": 0x96,
+            "g": 0x4B,
+            "b": 0x95
+        },
         "elements": [
             {
                 "elementType": "quadraticCurve",
@@ -113,7 +97,11 @@ var figure11 = [
         ]
     },
     {
-        "color": "#eae300",
+        "color": {
+            "r": 0xEA,
+            "g": 0xE3,
+            "b": 0
+        },
         "elements": [
             {
                 "elementType": "line",
@@ -133,7 +121,6 @@ var figure11 = [
 
 var figure12 = [
     {
-        "color": "#eae300",
         "elements": [
             {
                 "elementType": "curveByPoints",
@@ -156,7 +143,6 @@ var figure12 = [
         ]
     },
     {
-        "color": "#e60000",
         "elements": [
             {
                 "elementType": "arc",
@@ -165,7 +151,6 @@ var figure12 = [
         ]
     },
     {
-        "color": "#964b95",
         "elements": [
             {
                 "elementType": "quadraticCurve",
@@ -218,7 +203,6 @@ var figure12 = [
         ]
     },
     {
-        "color": "#eae300",
         "elements": [
             {
                 "elementType": "line",
@@ -234,32 +218,6 @@ var figure12 = [
             }
         ]
     }
-];
-
-var figure2 = [
-    [
-        86, 105,
-        99, 90,
-        113, 79,
-        134, 68,
-        156, 64,
-        182, 70,
-        198, 81,
-        212, 93,
-        224, 108
-    ],
-    [
-        224, 108,
-        250, 107,
-        275, 110,
-        295, 117,
-        308, 126,
-        320, 138,
-        328, 150,
-        331, 158,
-        335, 167,
-        339, 187
-    ]
 ];
 
 var drawFigure = function (ctx, figure, clear) {
@@ -305,7 +263,10 @@ var drawFigure = function (ctx, figure, clear) {
             ctx.strokeStyle = "#000";
             ctx.lineWidth = 20;
         } else {
-            ctx.strokeStyle = curFig.color;
+            ctx.strokeStyle = 'rgb(' 
+                    + curFig.color.r + ','
+                    + curFig.color.g + ','
+                    + curFig.color.b + ')';
             ctx.lineWidth = 1;
         }
 
@@ -405,111 +366,8 @@ var linearTransformation = function (ctx, source, target, steps) {
         }, 300);
 }
 
-var drawCurve = function (ctx, points) {
-
-    var p_len = points.length;
-    if (p_len < 4) return;
-
-    ctx.moveTo(points[0], points[1]);
-    if (p_len < 6) {
-        ctx.lineTo(points[2], points[3]);
-        return;
-    }
-
-    for (var i = 2; i < p_len - 4; i += 2) {
-
-        var xc = (points[i] + points[i + 2]) / 2;
-        var yc = (points[i + 1] + points[i + 3]) / 2;
-        ctx.quadraticCurveTo(points[i], points[i + 1], xc, yc);
-    }
- 
-    ctx.quadraticCurveTo(points[i], points[i + 1], points[i + 2], points[i + 3]);
-};
-
-var getTransformFigures = function (figure_1, figure_2, step) {
-    
-    var getAllPoints = function (figure) {
-        var allPoints = [];
-        for (var i = 0; i < figure.length; i++) {
-            for (var j = 0; j < figure[i].length; j++) {
-                allPoints.push(figure[i][j]);
-            }
-        }
-        return allPoints;
-    };
-
-    var figures = [];
-    if (step < 1) return figures;
-    figures.push(figure_1);
-    if (step === 1) {
-        figures.push(figure_2);
-        return figures;
-    }
-
-    var points_1 = getAllPoints(figure_1);
-    var points_2 = getAllPoints(figure_2);
-    var deltas = [];
-    for (var i = 0; i < points_1.length; i++) {
-        deltas.push( (points_2[i] - points_1[i])/step );
-    }
-    for (var s = 1; s < step; s++) {
-        var new_figure = [];
-        var mid_points = [];
-        for (var i = 0; i < points_1.length; i++) {
-            mid_points.push(points_1[i] + deltas[i] * s);
-        }
-        new_figure.push(mid_points);
-        figures.push(new_figure);
-    }
-    figures.push(figure_2);
-    return figures;
-};
-
-var transformFigures = function (ctx, figures, delay, endProcessFunction) {
-
-    var clearFigure = function (fig) {
-        for (var i = 0; i < fig.length; i++) {
-            ctx.beginPath();
-            ctx.strokeStyle = "#fff";
-            ctx.lineWidth = 3;
-            drawCurve(ctx, fig[i]);
-            ctx.stroke();
-        }
-    };
-
-    var drawFigure = function (fig) {
-        for (var i = 0; i < fig.length; i++) {
-            ctx.beginPath();
-            ctx.strokeStyle = "#f00";
-            ctx.lineWidth = 1;
-            drawCurve(ctx, fig[i]);
-            ctx.stroke();
-        }
-    };
-
-    var steps = figures.length;
-    var transform = function () {
-        clearFigure(figures[figures.length - steps]);
-        drawFigure(figures[figures.length - steps + 1]);
-        steps--;
-        if (steps > 1) {
-            setTimeout(transform, delay);
-        } else {
-            if (typeof endProcessFunction === 'function') endProcessFunction();
-        }
-    };
-
-    setTimeout(transform, delay);
-}
-
 window.onload = function () {
     var ctx = document.getElementById("cnv").getContext("2d");
-    // drawCurves(ctx);
-    // var figures = getTransformFigures(figure1, figure2, 60);
-    // transformFigures(ctx, figures, 50, function () {
-    //     figures = getTransformFigures(figure2, figure1, 60);
-    //     transformFigures(ctx, figures, 50);
-    // });
 
     drawFigure(ctx, figure11);
     linearTransformation(ctx, figure11, figure12, 20);
